4c10972328d14fe0813c2f729486f01a176da685
Mohammed alnakli <hassoun@outlook.com>
2015-05-03 16:45:57 -0400

    clean up some code

--- app.rb
+++ app.rb
@@ -1,73 +1,48 @@
 require 'date'
-class Email
+require_relative 'emails'
 
-    attr_reader :body, :subject , :from, :date, :email_address
-
-
-    #: param [Hash] {subject:,body:,from:,date:,email_address}
-    def initialize(email)
-        @body = email[:body]
-        @subject = email[:subject]
-        @from = email[:from]
-        @date = email[:date]
-        @email_address = email[:email_address]
-
-    end
-
-    def body
-        @body
-    end
-    def subject
-        @subject
-    end
-    def from
-        @from
-    end
-
-    def date
-        @date
-    end
-    def email_address
-        @email_address
-    end
-    def show
-        " Email Address: #{email_address}\n Subject: #{subject} \n From: #{from} \n Date: #{date} \n\n--------------------------- \n Body: #{body}
-        \n"
+# :description it is called when the fetch button clicked
+def fetch_button_click(emails_list)
+    EmailOperations.fetch_emails_all.zip(emails_list).each do |fetch_email,list|
+        list.replace(link(fetch_email.email_address) {
+                         @current_email[:email_address] = fetch_email.email_address
+                         @current_email[:email] = fetch_email.show
+                         @email_box.text = @current_email[:email]
+                     },
+                     "\t \t ",
+                      link('Delete', size: 12) {
+                          delete_an_email(fetch_email)
+                      }
+        )
     end
 end
-
-fetch_email = Class.new do
-    # :return [Array] of emails
-    def all
-        emails = []
-
-        #sample
-        10.times do
-            emails.push(Email.new({body:'email body', subject:'email subject', date:'12 may', from:'email@address.com', email_address:'my@broker.ca'}))
-        end
-        emails
+def delete_an_email(email)
+    if confirm('Are you sure, cannot be undo')
+        alert email
     end
-end.new
+end
+def mark_an_email_as_reviewed(email)
+    alert email
+end
 
 Shoes.app title: 'Human Intervention', width: 1300, height: 660, resizable: false do
 
     @current_email = {} # keep all the information need to be save to master website
     @shipment = [] # keep the shipment information such as Ship and port name and Date
+    @emails_list = [] # keep the list of emails
+
+    # stack to show emails as a list
     stack  height:650  , width: 300  do
         border "#ffffff", strokewidth: 2, curve: 12
         button('Fetch')  do
-            fetch_email.all.each do |email|
-                para "\n"
-                para link(email.email_address) {
-                         @current_email[:email_address] = email.email_address
-                         @current_email[:email] = email.show
-                         @email_box.text = @current_email[:email]
-                     },"\t \t ",link('Delete') {
-
-                      }
-            end
+            fetch_button_click(@emails_list)
+        end
+        10.times do |i|
+            para "\n"
+           @emails_list.push(tagline )
         end
     end
+
     stack margin_left: 5, margin_top: 5, height:650, width:600, scroll: true  do
         border "#ffffff", strokewidth: 1, curve: 12
         @email_box = edit_box '',  margin: 5, height:640, width:590
@@ -110,6 +85,8 @@ Shoes.app title: 'Human Intervention', width: 1300, height: 660, resizable: fals
             end
         }
     end
+
+
 end
 
 
@@ -128,3 +105,5 @@ end
 # s.append do para 'You pressed ' + key.to_s end
 # p key
 # end
+
+# list_box items: ["Jack", "Ace", "Joker"]
---
+++ emails.rb
@@ -0,0 +1,56 @@
+# fetch emails from the servers
+class EmailOperations
+
+    class << self
+        # :return [Array] of [Email]. Only return 10 not reviewed emails at a time
+        def fetch_emails_all
+            emails = []
+
+            #sample
+            10.times do
+                emails.push(Email.new({body:'email body', subject:'email subject', date:'12 may', from:'email@address.com', email_address:'my@broker.ca'}))
+            end
+            emails
+        end
+    end
+
+end
+
+
+# email class
+class Email
+
+    attr_reader :body, :subject , :from, :date, :email_address
+
+
+    #: param [Hash] {subject:,body:,from:,date:,email_address}
+    def initialize(email)
+        @body = email[:body]
+        @subject = email[:subject]
+        @from = email[:from]
+        @date = email[:date]
+        @email_address = email[:email_address]
+
+    end
+
+    def body
+        @body
+    end
+    def subject
+        @subject
+    end
+    def from
+        @from
+    end
+
+    def date
+        @date
+    end
+    def email_address
+        @email_address
+    end
+    def show
+        " Email Address: #{email_address}\n Subject: #{subject} \n From: #{from} \n Date: #{date} \n\n--------------------------- \n Body: #{body}
+        \n"
+    end
+end
